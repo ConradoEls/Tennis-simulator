07bf8dcc41086754f6588095c2745cdcb2ec247b
Conrado Els <conradoels@gmail.com>
2023-07-31 10:46:03 -0300

    detalles en la suma de puntos

--- dux-tennis/src/main/java/com/example/duxtennis/dao/TournamentDAO.java
+++ dux-tennis/src/main/java/com/example/duxtennis/dao/TournamentDAO.java
@@ -42,19 +42,71 @@ public class TournamentDAO implements IDao<Tournament>{
                 while (gamePlayer1 < 6 && gamePlayer2 < 6) {
                     System.out.println();
                     System.out.println(" - " + firstServe + " tiene el saque: ");
+                    int poits = 0;
                     int pointsPlayer1 = 0;
                     int pointsPlayer2 = 0;
-                    while (pointsPlayer1 < 3 && pointsPlayer2 < 3) {
+                    while (poits < 3) {
                         if (rd.nextInt(100) < player1.getWinProb()) {
                             pointsPlayer1++;
                         } else {
                             pointsPlayer2++;
                         }
+                        poits++;
                         board.points(player1, player2, pointsPlayer1, pointsPlayer2);
                     }
+                    while (Math.abs(pointsPlayer1 - pointsPlayer2) < 2){
+                        if((pointsPlayer1 == 2 && pointsPlayer2 == 2) || (pointsPlayer1 == 1 && pointsPlayer2 == 2) || (pointsPlayer2 == 1 && pointsPlayer1 == 2)){
+                            if (rd.nextInt(100) < player1.getWinProb()) {
+                                pointsPlayer1++;
+                            } else {
+                                pointsPlayer2++;
+                            }
+                            board.points(player1, player2, pointsPlayer1, pointsPlayer2);
+                        } else if ((pointsPlayer1 == 3 && pointsPlayer2 == 2) || (pointsPlayer1 == 2 && pointsPlayer2 == 3)){
+                            if (rd.nextInt(100) < player1.getWinProb()) {
+                                pointsPlayer1++;
+                            } else {
+                                pointsPlayer2++;
+                            }
+                        } else if ((pointsPlayer1 == 3 && pointsPlayer2 == 1) || (pointsPlayer2 == 3 && pointsPlayer1 == 1)) {
+                            if (rd.nextInt(100) < player1.getWinProb()) {
+                                pointsPlayer1++;
+                            } else {
+                                pointsPlayer2++;
+                            }
+                        } else if (pointsPlayer1 == 3 && pointsPlayer2 == 3) {
+                            board.points(player1, player2, pointsPlayer1, pointsPlayer2);
+                            if (rd.nextInt(100) < player1.getWinProb()) {
+                                pointsPlayer1++;
+                            } else {
+                                pointsPlayer2++;
+                            }
+
+                        } else if ((pointsPlayer1 == 4 && pointsPlayer2 == 3)) {
+                            board.points(player1, player2, pointsPlayer1, pointsPlayer2);
+                            if (rd.nextInt(100) < player1.getWinProb()) {
+                                pointsPlayer1++;
+                            } else {
+                                pointsPlayer2++;
+                                pointsPlayer1--;
+                            }
+
+                        } else if ((pointsPlayer2 == 4 && pointsPlayer1 == 3)) {
+                            board.points(player1, player2, pointsPlayer1, pointsPlayer2);
+                            if (rd.nextInt(100) < player1.getWinProb()) {
+                                pointsPlayer1++;
+                                pointsPlayer2--;
+                            } else {
+                                pointsPlayer2++;
+                            }
+                        }
+                    }
+                    System.out.println();
                     if (pointsPlayer1 > pointsPlayer2) {
+                        System.out.println("- " + player1.getName() + " ganó el game!");
                         gamePlayer1++;
                     } else {
+                        System.out.println("- " + player2.getName() + " ganó el game!");
                         gamePlayer2++;
                     }
                     board.games(player1, player2, setsPlayer1, setsPlayer2, gamePlayer1, gamePlayer2);
